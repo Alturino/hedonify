01727e00a5277c7c320f357c828d77479107a50a
Ricky Alturino <alturino001@gmail.com>
2025-01-24 16:09:21 +0700

    feat(main): add utility function to retrieve and attach token to context

--- internal/common/token.go
+++ internal/common/token.go
@@ -5,44 +5,49 @@ import (
 	"fmt"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"github.com/rs/zerolog"
 
-	inErrors "github.com/Alturino/ecommerce/internal/common/errors"
+	"github.com/Alturino/ecommerce/internal/common/constants"
+	commonErrors "github.com/Alturino/ecommerce/internal/common/errors"
+	"github.com/Alturino/ecommerce/internal/common/otel"
 	"github.com/Alturino/ecommerce/internal/config"
 	"github.com/Alturino/ecommerce/internal/log"
 )
 
-func VerifyToken(c context.Context, token string) error {
+func VerifyToken(c context.Context, token string) (*jwt.Token, error) {
+	c, span := otel.Tracer.Start(c, "VerifyToken")
+	defer span.End()
+
 	logger := zerolog.Ctx(c).
 		With().
 		Str(log.KeyTag, "VerifyToken").
-		Str(log.KeyAuthToken, token).
 		Logger()
-
-	claims := jwt.RegisteredClaims{}
 
 	logger = logger.With().Str(log.KeyProcess, "initializing config").Logger()
 	logger.Info().Msg("initializing config")
 	c = logger.WithContext(c)
-	config := config.InitConfig(c, AppUserService)
+	config := config.InitConfig(c, constants.AppUserService)
 	logger.Info().Msg("initialized config")
 
 	logger = logger.With().Str(log.KeyProcess, "parsing claims").Logger()
+	logger.Info().Msg("parsing claims")
 	jwtToken, err := jwt.ParseWithClaims(token,
-		claims,
+		&jwt.RegisteredClaims{},
 		func(t *jwt.Token) (interface{}, error) {
-			return config.Application.SecretKey, nil
+			return []byte(config.SecretKey), nil
 		},
-		jwt.WithAudience(AudienceUser),
+		jwt.WithAudience(constants.AudienceUser),
 		jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Name}),
 		jwt.WithExpirationRequired(),
 		jwt.WithIssuedAt(),
-		jwt.WithIssuer(AppUserService),
+		jwt.WithIssuer(constants.AppUserService),
 	)
 	if err != nil {
-		err = fmt.Errorf("failed parsing with claims with error=%w", err)
+		err = fmt.Errorf("failed parsing claims with error=%w", err)
 		logger.Error().Err(err).Msg(err.Error())
-		return err
+		commonErrors.HandleError(err, span)
+		return nil, err
 	}
 	logger = logger.With().Any(log.KeyToken, jwtToken).Logger()
 	logger.Info().Msg("parsed claims")
@@ -50,10 +55,54 @@ func VerifyToken(c context.Context, token string) error {
 	logger = logger.With().Str(log.KeyProcess, "validating token").Logger()
 	logger.Info().Msg("validating token")
 	if !jwtToken.Valid {
-		logger.Error().Err(inErrors.ErrTokenInvalid).Msg(inErrors.ErrTokenInvalid.Error())
-		return inErrors.ErrTokenInvalid
+		err = fmt.Errorf("failed validating token with error=%w", commonErrors.ErrTokenInvalid)
+		commonErrors.HandleError(err, span)
+		logger.Error().Err(err).Msg(err.Error())
+		return nil, commonErrors.ErrTokenInvalid
 	}
 	logger.Info().Msg("validated token")
 
-	return nil
+	return jwtToken, nil
+}
+
+type jwtToken struct{}
+
+func AttachJwtToken(c context.Context, jwt *jwt.Token) context.Context {
+	return context.WithValue(c, jwtToken{}, jwt)
+}
+
+func JwtTokenFromContext(c context.Context) *jwt.Token {
+	return c.Value(jwtToken{}).(*jwt.Token)
+}
+
+func UserIdFromJwtToken(c context.Context) (uuid.UUID, error) {
+	c, span := otel.Tracer.Start(c, "UserIdFromJwtToken")
+	defer span.End()
+
+	logger := zerolog.Ctx(c).With().Logger()
+
+	logger = logger.With().Str(log.KeyProcess, "getting userId from jwtToken").Logger()
+	logger.Info().Msg("getting jwtToken from context")
+	jwt := JwtTokenFromContext(c)
+	subject, err := jwt.Claims.GetSubject()
+	if err != nil {
+		err = fmt.Errorf("failed getting subject from jwt with error=%w", err)
+		commonErrors.HandleError(err, span)
+		logger.Error().Err(err).Msg(err.Error())
+		return uuid.Nil, err
+	}
+	logger.Info().Msg("got subject from jwtToken")
+
+	logger.Info().Msg("parsing subject")
+	userId, err := uuid.Parse(subject)
+	if err != nil {
+		err = fmt.Errorf("failed parsing subject=%s with error=%w", subject, err)
+		commonErrors.HandleError(err, span)
+		logger.Error().Err(err).Msg(err.Error())
+		return uuid.Nil, err
+	}
+	logger = logger.With().Str(log.KeyUserID, userId.String()).Logger()
+	logger.Info().Msg("parsed subject as userId")
+
+	return userId, nil
 }
