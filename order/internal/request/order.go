b1414189d843a5e6c86366603a7ab190b7e89b76
Ricky Alturino <alturino001@gmail.com>
2025-01-24 16:28:15 +0700

    chore(order): add struct request and response

--- order/internal/request/order.go
+++ order/internal/request/order.go
@@ -1,28 +1,19 @@
 package request
 
-import "github.com/google/uuid"
-
-type InsertOrderRequest struct {
-	OrderItems []OrderItemRequest `validate:"required"      json:"orderItems"`
-	CartId     uuid.UUID          `validate:"required,uuid" json:"cartId"`
-	UserId     uuid.UUID          `validate:"required,uuid" json:"userId"`
-}
-
-type OrderItemRequest struct {
-	Price     string    `validate:"required"       json:"price"`
-	Quantity  int       `validate:"required,gte=1" json:"quantity"`
-	ProductId uuid.UUID `validate:"required,uuid"  json:"productId"`
-}
+import (
+	"github.com/google/uuid"
+)
 
 type FindOrderByUserId struct {
 	UserId uuid.UUID `validate:"uuid"`
 }
 
 type FindOrderById struct {
+	UserId  uuid.UUID `validate:"required,uuid"`
 	OrderId uuid.UUID `validate:"required,uuid"`
 }
 
 type FindOrders struct {
-	UserId  string
-	OrderId string
+	UserId  uuid.UUID `validate:"required,uuid"`
+	OrderId uuid.UUID `validate:"required,uuid"`
 }
