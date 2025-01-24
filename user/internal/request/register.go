67c60533a03f77b72c3c371d6944ff5bde39656e
Ricky Alturino <alturino001@gmail.com>
2025-01-24 16:25:24 +0700

    chore(user): add struct register request/register.go

--- user/internal/request/register.go
+++ user/internal/request/register.go
@@ -1,7 +1,13 @@
 package request
 
-type RegisterRequest struct {
+import "github.com/google/uuid"
+
+type Register struct {
 	Username string `validate:"required"       json:"username"`
 	Email    string `validate:"required,email" json:"email"`
 	Password string `validate:"required"       json:"password"`
+}
+
+type FindUserById struct {
+	ID uuid.UUID `validate:"required,uuid" json:"id"`
 }
