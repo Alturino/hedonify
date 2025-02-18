c257e0fa0ba7cd682b39457749484cb4075ed150
Ricky Alturino <alturino001@gmail.com>
2025-02-18 12:40:17 +0700

    chore(main): remove unnecessary step for load testing

--- k6/src/orderCreation.ts
+++ k6/src/orderCreation.ts
@@ -13,15 +13,11 @@ export const options = {
   duration: "5s",
 };
 
-const registers = new SharedArray("registers", () => {
-  return JSON.parse(open("./registers.json"));
-});
-
 const users = new SharedArray("users", () => {
   return JSON.parse(open("./users.json"));
 });
 
-const productsSeed = new SharedArray("products", () => {
+const products = new SharedArray("products", () => {
   return JSON.parse(open("./products.json"));
 });
 
@@ -31,21 +27,6 @@ const counterOrderFail = new Counter("order_fail");
 export default function () {
   // randomize user
   const userRandomIndex = Math.floor(Math.random() * users.length);
-  const register = registers[userRandomIndex];
-  const registerResponse = http
-    .post("http://localhost/users/register", JSON.stringify(register), {})
-    .json();
-  if (
-    !check(registerResponse, {
-      "Success registering user": (r) =>
-        r?.statusCode === 200 || r?.statusCode === 409,
-    })
-  ) {
-    console.error(`registerResponse=${JSON.stringify(registerResponse)}`);
-    fail("Failed to register user");
-  }
-  console.log(`registerResponse=${JSON.stringify(registerResponse)}`);
-
   const user = users[userRandomIndex];
   const loginResponse = http
     .post("http://localhost/users/login", JSON.stringify(user), {})
@@ -60,45 +41,12 @@ export default function () {
   console.log(`loginResponse=${JSON.stringify(loginResponse)}`);
   const token = loginResponse.data.token;
 
-  // insert products
-  const insertProductRequest = [];
-  for (const product of productsSeed) {
-    insertProductRequest.push({
-      method: "POST",
-      url: "http://localhost/products",
-      body: JSON.stringify(product),
-      params: {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      },
-    });
-  }
-  console.log(`insertProductRequest=${JSON.stringify(insertProductRequest)}`);
-  const response = http.batch(insertProductRequest).map((r) => r.json());
-  console.log(`insertProductResponse=${JSON.stringify(response)}`);
-
   // randomize product selection
-  const productResponse = http
-    .get("http://localhost/products", {
-      headers: { "Content-Type": "application/json" },
-    })
-    .json();
-  const isProductSuccess = check(productResponse, {
-    "Success get products": (r) => r?.statusCode === 200,
-  });
-  if (!isProductSuccess) {
-    console.error(`productResponse=${JSON.stringify(productResponse)}`);
-    fail("Failed get products");
-  }
-  console.log(`productResponse=${JSON.stringify(productResponse)}`);
   const cartItems = [];
-  const products = productResponse?.data.products;
-  const randomOrderItemCount = Math.floor(
+  const randomItemCount = Math.floor(
     Math.random() * (maxOrderItems - minOrderItems) + minOrderItems,
   );
-  for (let i = 0; i < randomOrderItemCount; i++) {
+  for (let i = 0; i < randomItemCount; i++) {
     const product = products[Math.floor(Math.random() * products.length)];
     const newProduct = {
       price: product.price,
