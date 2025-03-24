bfe70a7a27c3a39aa9fdfd1dbf545842672cd58d
Ricky Alturino <alturino001@gmail.com>
2025-03-24 23:44:12 +0700

    chore(main): change port from 8080 to 80

--- internal/constants/service_url.go
+++ internal/constants/service_url.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	URL_PRODUCT_SERVICE = "http://product-service:8080/products"
-	URL_USER_SERVICE    = "http://user-service:8080/users"
-	URL_ORDER_SERVICE   = "http://order-service:8080/orders"
+	URL_PRODUCT_SERVICE = "http://product-service/products"
+	URL_USER_SERVICE    = "http://user-service/users"
+	URL_ORDER_SERVICE   = "http://order-service/orders"
 )
--- internal/http/constants.go
+++ internal/http/constants.go
@@ -5,9 +5,3 @@ const (
 	VALUE_HEADER_APPLICATION_JSON = "application/json"
 	KEY_HEADER_REQUEST_ID         = "X-REQUEST-ID"
 )
-
-const (
-	PRODUCT_BASE_URL = "http://product-service:8080/products"
-	CART_BASE_URL    = "http://cart-service:8080/carts"
-	ORDER_BASE_URL   = "http://order-service:8080/orders"
-)
